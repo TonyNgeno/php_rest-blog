2c6f79ab451e7dc3516b300e3b7e4d0c26e94e8d
TonyNgeno <tonyngeno123@gmail.com>
2019-01-11 13:42:38 +0300

    initial commit

---
+++ config/Database.php
@@ -0,0 +1,9 @@
+<?php
+ class Database {
+     //param for db
+     private $host = 'localhost';
+     private $db_name = 'myblog';
+     private $username = 'root';
+     private $password = '123456'
+
+ }
